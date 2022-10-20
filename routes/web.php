36a931e5ccdd60e7c0d4f90f5f7f740d5f640c3c
Sergei <sergei2022gb@mail.ru>
2022-10-20 03:11:06 +0300

    [lesson-1] Lesson #1 task

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return
+        "<H1>Lesson #1</H1>
+        <ul>
+            <li><a href='/profile/'>Profile</a></li>
+            <li><a href='/about/'>About us</a></li>
+            <li><a href='/news/'>News</a></li>
+        </ul>";
+});
+
+Route::get('/about/', function () {
+    return "
+    <H1>About us</H1>
+    Coming soon...";
+});
+
+
+Route::get('/profile/', function () {
+    //TODO: get a user information
+    $userName = "<Unregistered \"User\">";
+    return "<H1>Profile</H1>
+    Greetings, " . htmlspecialchars($userName) . "!";
+});
+
+Route::get('/news', function () {
+    return
+        "<H1>Our news</H1>
+        <ul>
+            <li><a href='/news/1'>News #1</a></li>
+            <li><a href='/news/11'>News #11</a></li>
+            <li><a href='/news/123'>News #123</a></li>
+        </ul>";
+});
+
+Route::get('/news/{newsElement}', function (string $newsElement) {
+    $newsElementId = intval($newsElement);
+    if ($newsElementId <= 0) {
+        return "<i>Not found</i>";
+    }
+    //TODO: get a news information
+    return "
+    <H1>News #" . $newsElementId . "</H1>
+    Some texts about news #" . $newsElementId . "...";
 });
